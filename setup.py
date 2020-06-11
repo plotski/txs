39fdac43fd3fe081c62da89a34d3a27875ac8f70
plotski <plotski@mailbox.org>
2020-06-11 14:52:18 +0200

    Don't import txs in setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,21 @@
 from setuptools import setup, find_packages
-import txs
+import re
+
+def get_var(name):
+    with open('txs/__init__.py') as f:
+        content = f.read()
+        match = re.search(rf'''^__version__\s*=\s*['"]([^'"]*)['"]''',
+                          content, re.MULTILINE)
+        if match:
+            return match.group(1)
+        else:
+            raise RuntimeError('Unable to find __version__')
 
 setup(
-    name=txs.__name__,
-    version=txs.__version__,
-    author=txs.__author__,
-    author_email=txs.__author_email__,
+    name='txs',
+    version=get_var('__version__'),
+    author=get_var('__author__'),
+    author_email=get_var('__author_email__'),
     description='Wrapper around ffmpeg and mpv that generates and compares x264 test encodes',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
